8e4d71f956afb5a827f5099881349df71a4a4893
bbshark99 <andy.solution99@gmail.com>
2021-12-10 06:52:20 -0500

    Initial commit

---
+++ src/utils/markets.ts
@@ -0,0 +1,24 @@
+import Web3 from 'web3'
+
+export const getConditionId = (
+  oracleAddress: string,
+  questionId: string,
+  outcomeSlotCount: number,
+) => {
+  return Web3.utils.soliditySha3(
+    { t: 'address', v: oracleAddress },
+    { t: 'bytes32', v: questionId },
+    { t: 'uint', v: outcomeSlotCount },
+  )
+}
+
+export const getPositionId = (collateralToken: string, collectionId: string) => {
+  return Web3.utils.soliditySha3(
+    { t: 'address', v: collateralToken },
+    { t: 'bytes32', v: collectionId },
+  )
+}
+
+export const getHexFromNumber = (num: number) => {
+  return `0x${(num * Math.pow(10, 18)).toString(16)}`
+};
