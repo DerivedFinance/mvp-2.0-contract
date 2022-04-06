e388f840aa07b4b576ecade461a042e44ad25766
bbshark99 <andy.solution99@gmail.com>
2022-04-05 22:05:42 -0500

    deploy on bsc

--- scripts/deploy-market.js
+++ scripts/deploy-market.js
@@ -3,7 +3,12 @@ const hre = require("hardhat");
 
 async function main() {
   // const usdx = "0x64544969ed7EBf5f083679233325356EbE738930";
-  const usdx = "0x4a5F200B65DC4da50c7Ccf81b7bcF34c33BaF632";
+
+  // rinkeby
+  // const usdx = "0x4a5F200B65DC4da50c7Ccf81b7bcF34c33BaF632";
+
+  // bsctest
+  const usdx = "0xfADF4D058bFD10A4aa14739d96e211693F5F295b";
   // Deploy SharkNFT contract
   const DerivedPredictionMarket = await hre.ethers.getContractFactory(
     "DerivedPredictionMarket"
