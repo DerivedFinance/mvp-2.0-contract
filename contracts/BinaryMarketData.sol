96130a075f2a58c3ccc16a812411e3cbe0c38fc4
Bharat <bharatverma@Bharats-MacBook-Air.local>
2022-04-29 12:16:30 -0700

    rentry guard, emergency withdraw function

--- contracts/BinaryMarketData.sol
+++ contracts/BinaryMarketData.sol
@@ -5,55 +5,55 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 pragma solidity ^0.8.0;
 
 contract BinaryMarketData {
-  struct Question {
-    string meta;
-    uint256 resolveTime;
-    uint256 initialLiquidity;
-    uint8 fee;
-    uint8 slot;
-  }
+    struct Question {
+        string meta;
+        uint256 resolveTime;
+        uint256 initialLiquidity;
+        uint8 fee;
+        uint8 slot;
+    }
 
-  struct Market {
-    uint256 slot1;
-    uint256 slot2;
-    uint256 volume;
-    uint256 reward;
-  }
+    struct Market {
+        uint256 slot1;
+        uint256 slot2;
+        uint256 volume;
+        uint256 reward;
+    }
 
-  Question[] public questions;
+    Question[] public questions;
 
-  /**
-   * @notice Market Data
-   * @dev questionId => market data
-   */
-  mapping(uint256 => Market) public markets;
+    /**
+     * @notice Market Data
+     * @dev questionId => market data
+     */
+    mapping(uint256 => Market) public markets;
 
-  mapping(uint256 => uint256) public tradeFees;
+    mapping(uint256 => uint256) public tradeFees;
 
-  IERC20 public token;
+    IERC20 public token;
 
-  event QuestionCreated(
-    string title,
-    string meta,
-    string category,
-    uint256 questionId,
-    uint256 resolveTime,
-    uint256 initialLiquidity,
-    uint8 fee
-  );
+    event QuestionCreated(
+        string title,
+        string meta,
+        string category,
+        uint256 questionId,
+        uint256 resolveTime,
+        uint256 initialLiquidity,
+        uint8 fee
+    );
 
-  event QuestionResolved(uint256 questionId, uint8 slot);
+    event QuestionResolved(uint256 questionId, uint8 slot);
 
-  event RecoveredFunds(uint256 amount);
+    event Trade(
+        uint256 questionId,
+        uint256 slot1,
+        uint256 slot2,
+        uint256 tokensAmount,
+        uint256 sharesAmount,
+        uint8 slot,
+        uint8 trade,
+        address trader
+    );
 
-  event Trade(
-    uint256 questionId,
-    uint256 slot1,
-    uint256 slot2,
-    uint256 tokensAmount,
-    uint256 sharesAmount,
-    uint8 slot,
-    uint8 trade,
-    address trader
-  );
+    event RecoveredFunds(uint256 amount, address _sent);
 }
