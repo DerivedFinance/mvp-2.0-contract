48c9cb5ba95295e96e85f7d0fca5201fc4b0edf1
bbshark99 <andy.solution99@gmail.com>
2022-05-15 18:46:43 -0500

    feat(proxy): added BinaryMarket proxy deploy

--- scripts/deploy-proxy-bm.js
+++ scripts/deploy-proxy-bm.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const hre = require("hardhat");
+const { ethers, upgrades } = require("hardhat");
 
 async function main() {
   // const usdx = "0x64544969ed7EBf5f083679233325356EbE738930";
@@ -11,40 +11,90 @@ async function main() {
   const usdc = "0x8c9C613DA9adBe462F71E273C8284610E6F57247";
 
   // Deploy Proxy contract
-  const ProxyContract = await hre.ethers.getContractFactory("Proxy");
-  const proxyContract = await ProxyContract.deploy();
+  // const ProxyContract = await hre.ethers.getContractFactory("Proxy");
+  // const proxyContract = await ProxyContract.deploy();
 
-  await proxyContract.deployed();
-  console.log(`Proxy Contract deployed: ${proxyContract.address}`);
+  // await proxyContract.deployed();
+  // console.log(`Proxy Contract deployed: ${proxyContract.address}`);
 
   // Deploy Binary Market contract
-  const BinaryMarket = await hre.ethers.getContractFactory("BinaryMarket");
-  const binaryMarket = await BinaryMarket.deploy();
+  const BinaryMarket = await ethers.getContractFactory("BinaryMarket");
+  // const binaryMarket = await BinaryMarket.deploy();
 
-  await binaryMarket.deployed();
-  console.log(`BinaryMarket deployed: ${binaryMarket.address}`);
+  // await binaryMarket.deployed();
+  // console.log(`BinaryMarket deployed: ${binaryMarket.address}`);
+
+  const proxyBinaryMarket = await upgrades.deployProxy(BinaryMarket, [usdc]);
+  await proxyBinaryMarket.deployed();
+
+  console.log(
+    "ProxyBinaryMarket contract deployed: ",
+    proxyBinaryMarket.address
+  );
 
   // Set implementation contract for Proxy
-  const setImplementationTx = await proxyContract.setImplementation(binaryMarket.address);
-  await setImplementationTx.wait();
-  console.log("Implementation contract is set.");
+  // const setImplementationTx = await proxyContract.setImplementation(
+  //   binaryMarket.address
+  // );
+  // await setImplementationTx.wait();
+  // console.log("Implementation contract is set.");
 
-  // Initialize binary market
-  const [owner] = await ethers.getSigners();
-  const abi = ["function initialize(address payable _token, address _owner, address payable _proxy) public"];
-  const proxied = new ethers.Contract(proxyContract.address, abi, owner);
+  // const testOwner = await proxyContract.owner();
+  // console.log("DEBUG-testOwner", testOwner);
 
-  const initializeTx = await proxied.initialize(usdc, owner.address, proxyContract.address);
-  await initializeTx.wait();
-  console.log("Initialization for Implementation executed.")
+  // // Initialize binary market
+  // const [owner] = await hre.ethers.getSigners();
+  // // console.log("DEBUG-owner", owner);
+
+  // const proxied = new hre.ethers.Contract(
+  //   proxyContract.address,
+  //   [
+  //     {
+  //       inputs: [
+  //         {
+  //           internalType: "address",
+  //           name: "_token",
+  //           type: "address",
+  //         },
+  //       ],
+  //       name: "initialize",
+  //       outputs: [],
+  //       stateMutability: "nonpayable",
+  //       type: "function",
+  //     },
+  //     {
+  //       inputs: [],
+  //       name: "owner",
+  //       outputs: [
+  //         {
+  //           internalType: "address",
+  //           name: "",
+  //           type: "address",
+  //         },
+  //       ],
+  //       stateMutability: "view",
+  //       type: "function",
+  //     },
+  //   ],
+  //   owner
+  // );
+
+  // const ttOwner = await proxied.owner();
+  // console.log("DEBUG-ttOwner", ttOwner);
+
+  // console.log("DEBUG-USDC: ", usdc);
+  // const initializeTx = await proxied.initialize(usdc);
+  // // const initializeTx = await proxyContract.initialize(usdc);
+  // await initializeTx.wait();
+  // console.log("Initialization for Implementation executed.");
 
   // await hre.run("verify:verify", {
   //   address: binaryMarket.address,
   //   constructorArguments: [],
   // });
-  console.log(
-    `npx hardhat verify ${binaryMarket.address} --network bsctest && npx hardhat verify ${proxyContract.address} --network bsctest`
-  );
+  // console.log(
+  //   `npx hardhat verify ${binaryMarket.address} --network bsctest && npx hardhat verify ${proxyContract.address} --network bsctest`
+  // );
 }
 
 main()
